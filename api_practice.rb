63b57bdf705dd16905176d88081d81f4e4d50ecd
jnappy <jpnappy@gmail.com>
2015-06-28 15:43:01 -0400

    added some guidance for api practice

---
+++ api_practice.rb
@@ -0,0 +1,28 @@
+require 'pry'
+require 'rest-client'
+require 'json'
+
+def get_reddit_info(subreddit)
+
+  url = "http://reddit.com/r/#{subreddit}.json"
+
+  response = RestClient.get(url)
+
+  parsed_response = JSON.parse(response)
+
+  posts = parsed_response['data']['children']
+
+  # Take ten posts from the posts variable (might have to Google it)
+
+  # Iterate over each of the posts 
+    # Put the title to the screen
+    # Put the url of the picture to the screen
+    # Put a new line
+end
+
+puts 'What subreddit would you like to crawl?'
+subreddit = gets.strip
+
+get_reddit_info(subreddit)
+
+
